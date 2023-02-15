37da5afeef754d8fe35bad8342f1fc733303eeca
Jesse Corbett <jesse@projectronin.com>
2023-02-14 22:42:22 -0800

    Remove custom plugin entirely

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // because the linter is unhappy about plugins owning the libs
 plugins {
     alias(libs.plugins.kotlin)
-    id("com.projectronin.demo.plugin") version "1.0.0-SNAPSHOT"
     application
 }
 
@@ -13,12 +12,6 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
-}
-
-
-// Configures an existing Task
-listFiles {
-    includeSizes.set(true)
 }
 
 
