da48280d4d4c87859f5acd7155a83ee36137f81c
Jesse Corbett <jesse@projectronin.com>
2023-02-14 22:42:55 -0800

    Remove application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,12 @@
 @Suppress("DSL_SCOPE_VIOLATION") // because the linter is unhappy about plugins owning the libs
 plugins {
     alias(libs.plugins.kotlin)
-    application
 }
 
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
-}
-
-application {
-    mainClass.set("MainKt")
 }
 
 
